589984072f77b14fcd232960f8976b5a782648bd
Arnold Krausz <arnoldx17@croyz.eu>
2026-06-05 07:56:20 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -5,22 +5,31 @@ from flask import Flask, request
 docuseal.key = "xHPyWnY8iyRLuTG9XuRjYZWKA1tAqSAnnwKa27YzYcT"
 docuseal.url = "https://servis.sevenet.sk/api/"
 
-response = docuseal.get_submission_documents(40)
-pdf_url = response['documents'][0]['url']
-name = response['documents'][0]['name']
-
-# PDF letöltése a tmp mappába
-subprocess.run(['wget', pdf_url, '-O', f'tmp/{name}.pdf'])
-
-print(f"PDF letöltve: tmp/{name}.pdf")
-
 # Flask webhook listener
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
     data = request.get_json()
+    submission_id = data.get('data', {}).get('submission_id')
+
+    email = None
+    for item in data.get('data', {}).get('values', []):
+        if item.get('field') == 'Email':
+            email = item.get('value')
+            break
+
+    response = docuseal.get_submission_documents(submission_id)
+    pdf_url = response['documents'][0]['url']
+    name = response['documents'][0]['name']
+
+    # PDF letöltése a tmp mappába
+    subprocess.run(['wget', pdf_url, '-O', f'tmp/{name}.pdf'])
+
     print("Webhook kapott:", data)
+    print(f"submission_id: {submission_id}")
+    print(f"email: {email}")
+    print(f"PDF letöltve: tmp/{name}.pdf")
     return {'status': 'ok'}, 200
 
 if __name__ == '__main__':

d4c532c35a7b561b45e8f25d941b597aaf46b0db
Arnold Krausz <arnoldx17@croyz.eu>
2026-06-05 07:27:30 +0000

    modified:   app.py

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from docuseal import docuseal
+import subprocess
 
 docuseal.key = "xHPyWnY8iyRLuTG9XuRjYZWKA1tAqSAnnwKa27YzYcT"
 docuseal.url = "https://servis.sevenet.sk/api/"
 
 response = docuseal.get_submission_documents(40)
 pdf_url = response['documents'][0]['url']
-print(pdf_url)
+name = response['documents'][0]['name']
+
+# PDF letöltése a tmp mappába
+subprocess.run(['wget', pdf_url, '-O', f'tmp/{name}.pdf'])
+
+print(f"PDF letöltve: tmp/{name}.pdf")
